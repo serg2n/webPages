bd8cc28fc4d1fbfc9aeb319723468d8ffad97e4a
Serg <box10@stim.by>
2018-10-14 18:18:03 +0300

    Добавил методы в Razor Pages

---
+++ webPages/Pages/Factorial.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webPages.Pages
+{
+    public class FactorialModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "Введите число";
+        }
+        public void OnPost(int? number)
+        {
+            if (number == null || number < 1)
+            {
+                Message = "Передано некорректное число. Повторите ввод";
+            }
+            else
+            {
+                int result = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    result *= i;
+                }
+                Message = $"Факториал числа {number} равен {result}";
+            }
+        }
+    }
+}
---
+++ webPages/Pages/Person.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using webPages.Models;
+using webPages.Pages;
+
+
+
+namespace webPages.Pages
+{
+    public class PersonModel : PageModel
+    {
+        List<Person> people;
+        public Person Person { get; set; }
+        public PersonModel()
+        {
+            people = new List<Person>()
+            {
+                new Person{ Name="Tom", Age=23},
+                new Person {Name = "Sam", Age=25},
+                new Person {Name="Bob", Age=23},
+                new Person{Name="Tom", Age=25}
+            };
+        }
+
+        public IActionResult OnGet(string name)
+        {
+            Person = people.FirstOrDefault(p => p.Name == name);
+            if (Person == null)
+                return RedirectToPage("Error");
+
+            return Page();
+        }
+    }
+}
